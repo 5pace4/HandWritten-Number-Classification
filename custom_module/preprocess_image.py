144f37ec1e208f1a01e461cabcd80df62a2e84d1
tofayelahmmedbabu <tofayelahmmedbabu@gmail.com>
2024-01-21 05:53:35 +0600

    f

--- custom_module/preprocess_image.py
+++ custom_module/preprocess_image.py
@@ -1,15 +1,12 @@
-import cv2
-import numpy as np
 from PIL import Image
+import numpy as np
 
 def preprocess_image(img):
-    img_gray = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-    blurred = cv2.GaussianBlur(img_gray, (5, 5), 0)
-    
-    # Thresholding using Otsu's method
-    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    
-    # Convert to PIL Image
-    thresh_pil = Image.fromarray(thresh)
-    
-    return thresh_pil
+    if isinstance(img, np.ndarray):
+        img = Image.fromarray(img)
+
+    img_gray = img.convert('L')
+    img_array = np.array(img_gray)
+    thresholded_array = (img_array > 127) * 255
+    thresholded_img = Image.fromarray(thresholded_array.astype('uint8'))
+    return thresholded_img
